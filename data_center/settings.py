ff781eec5e457bbf4049d84830a908051b338668
cdqr <cdqr8969@163.com>
2023-06-13 10:55:46 +0800

    testoption trun false

--- data_center/settings.py
+++ data_center/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 import os
 
 # 用来区分是否是测试环境
-TESTOPTION = True
+TESTOPTION = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
